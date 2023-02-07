bc60e2effe82195abd750a0829a8e57a5b92d0d8
Yjn024 <61787533+JieningYu@users.noreply.github.com>
2023-02-07 15:27:17 +0800

    l

--- src/main/java/com/netheriteqf/your_ideas/mixin/TitleScreenMixin.java
+++ src/main/java/com/netheriteqf/your_ideas/mixin/TitleScreenMixin.java
@@ -1,20 +1,24 @@
 package com.netheriteqf.your_ideas.mixin;
 
-import com.netheriteqf.your_ideas.YourIdeas;
-import com.netheriteqf.your_ideas.web.BrowserUrl;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.TexturedButtonWidget;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Identifier;
+import java.net.URL;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.netheriteqf.your_ideas.YourIdeas;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 
 /**
  * @author Goulixiaoji
@@ -42,9 +46,9 @@ public abstract class TitleScreenMixin extends Screen {
                                     String code = MinecraftClient.getInstance().getLanguageManager().getLanguage()
                                             .getCode();
                                     if (code.indexOf("zh") == 0) {
-                                        BrowserUrl.browserUrl("https://www.mcmod.cn/class/9072.html");
+                                        Util.getOperatingSystem().open(new URL("https://www.mcmod.cn/class/9072.html"));
                                     } else {
-                                        BrowserUrl.browserUrl("https://github.com/Netherite-QF/YourIdeas");
+                                        Util.getOperatingSystem().open(new URL("https://github.com/Netherite-QF/YourIdeas"));
                                     }
 
                                 } catch (Exception e) {
